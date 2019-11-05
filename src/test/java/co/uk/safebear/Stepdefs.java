e6be9d7203c29ebb5a9c8201ea16bb84711278b1
student <student@safebear.co.uk>
2019-11-05 16:12:24 +0000

    Stepdefs update

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -4,6 +4,9 @@ package co.uk.safebear;
 import co.uk.safebear.utils.Driver;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 
 public class Stepdefs {
@@ -26,6 +29,24 @@ public class Stepdefs {
         }
 
         browser.quit();
+    }
+
+    @Given("I am logged out")
+    public void i_am_logged_out() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter username {string} and password {string}")
+    public void i_enter_username_and_password(String username, String password) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the user is informed that the login in successful")
+    public void the_user_is_informed_that_the_login_in_successful() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
 
 }
