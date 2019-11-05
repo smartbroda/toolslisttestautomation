d7b8180a6a31086043991dce4fbedb05354aff4d
student <student@safebear.co.uk>
2019-11-05 15:35:20 +0000

    Stepdefs ChromeDriver

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -1,30 +1,31 @@
 package co.uk.safebear;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 
-import static org.junit.Assert.*;
+import co.uk.safebear.utils.Driver;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
 
 public class Stepdefs {
+    WebDriver browser;
 
-    @Given("I am logged out")
-    public void i_am_logged_out() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    @Before
+    public void setUp() {
+        // Get the driver for the browser (e.g. ChromeDriver)
+        browser = Driver.getDriver();
+        //navigate to the URL of our webpage
+        browser.navigate().to(Driver.getURL());
     }
 
-    @When("I enter username {string} and password {string}")
-    public void i_enter_username_and_password(String username, String password) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
+    @After
+    public void tearDown() {
+        try {
+            Thread.sleep(Integer.parseInt(System.getProperty("sleep", "2000")));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-    @Then("the user is informed that the login in successful")
-    public void the_user_is_informed_that_the_login_in_successful() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        browser.quit();
     }
-
 
 }
