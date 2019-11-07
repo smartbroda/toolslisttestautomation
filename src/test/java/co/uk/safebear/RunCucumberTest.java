e4f4def74a85a5f5790f4b0f535b8085a711829a
student <student@safebear.co.uk>
2019-11-07 13:49:26 +0000

    Reporting set up

--- src/test/java/co/uk/safebear/RunCucumberTest.java
+++ src/test/java/co/uk/safebear/RunCucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"json:target/cucumber-report/cucumber.json"},
-tags = "@runThis"
+tags = "@high-impact"
 // glue = "path to Stepdefs if not in the same folder",
 // features = "path to features if not in co.uk.safebear folder"
         )
